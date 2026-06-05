7a934bf28f14b07c7beed3b4e0b5ac030bb5cc9a
BamgasiJM <ryoong1125@gmail.com>
2026-06-05 22:12:54 +0900

    update css

--- src/data/animations.js
+++ src/data/animations.js
@@ -1,14 +1,17 @@
 export const animations = [
   {
-    title: "Basic Primitives1",
+    title: "Basic Primitives 1",
     file: "Basic_Primitives.json",
     figure: "Fig. 1.1",
-    description: "Basic geometric primitives created with Cavalry.",
+    description: "Background Shape위에 Stroke만 있는 Ellipse와 Fill만 있는 Ellipse를 각각 만들고 " +
+      "Duplicator에 이 두 개의 오브젝트를 등록." +
+  "Input Shapes의 순서를 변경할 수도 있고, Distribution 종류와 Count 등으로 레이아웃 변경" +
+    "Shape Time Offset에서 Add Behaviour > Stagger를 이용해 애니메이션 오프셋을 설정."
   },
   {
-    title: "Basic Primitives3",
+    title: "Basic Primitives 2",
     file: "Basic_Primitives.json",
-    figure: "Fig. 1.1",
+    figure: "Fig. 2.1",
     description: "Basic geometric primitives created with Cavalry.",
   },
 ];
